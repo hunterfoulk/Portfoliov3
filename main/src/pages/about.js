e2f97175dea950344cc98284319c631cfb63cce6
hunter foulk <huntertehjakey@hotmail.com>
2020-10-23 13:11:00 -0700

    updates

--- main/src/pages/about.js
+++ main/src/pages/about.js
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react'
 import "./about.scss"
 import Navbar from "../components/navbar"
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+import Header from "../components/header"
+import Footer from "../components/footer"
+
 import { GlobalDispatchContext, GlobalStateContext, } from "../context/GlobalContextProvider"
 
 
