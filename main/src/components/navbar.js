5cab1655c91e85554c043cbc24c24edbc6f7af44
hunter foulk <huntertehjakey@hotmail.com>
2020-10-23 13:23:38 -0700

    updates

--- main/src/components/navbar.js
+++ main/src/components/navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import "./navbar.scss"
 import Layout from "../components/layouts/layout"
 import { Router, Link } from "@reach/router"
@@ -7,17 +7,17 @@ import { Router, Link } from "@reach/router"
 export default function Navbar() {
 
     const [dropdown, setDropdown] = useState(false)
-
-
     const [isMobile, conclusion] = useState(false)
+    const [newPath, setNewPath] = useState("")
 
     useEffect(() => {
         const isMobile = window.innerWidth < 700
         conclusion(isMobile)
+        const fullPath = window.location.pathname;
+        setNewPath(fullPath)
     }, [])
 
-    const fullPath = window.location.pathname;
-    console.log("this is the full Path", fullPath)
+
 
 
 
@@ -47,7 +47,7 @@ export default function Navbar() {
                     <span>About</span>
                 </Link>
 
-                    {fullPath === "/" ? <span onClick={() => {
+                    {newPath === "/" ? <span onClick={() => {
                         window.scrollTo(740, 740);
                     }}>Projects</span> : <Link style={{ textDecoration: "none", outline: "none", color: "#23272a" }} to="/">
                             <span>Projects</span>
