7d47459b65b9d04b28bd9b5f6f631c52c5a9e42e
hunter foulk <huntertehjakey@hotmail.com>
2020-11-24 10:58:58 -0700

    header change

--- main/src/components/header.js
+++ main/src/components/header.js
@@ -10,7 +10,7 @@ export default function Header() {
         <div className="header-container">
             <div className="header-left">
                 <h1>Hi, I'm Hunter!</h1>
-                <span>I'm a Software Developer specialized in front end and back end development and has a passion for developing complex scalable web apps.</span>
+                <span>I'm a Software Developer specialized in front end and back end development and have a passion for developing complex scalable web apps.</span>
             </div>
             <div className="header-right">
                 <div className="image-container">
